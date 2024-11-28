ec7d577ff02cb02185c9a7ffe239483370905de6
TC <nothing@nothing.com>
2024-11-28 16:30:00 +1300

    Add '=' key functionality

--- code.js
+++ code.js
@@ -1,8 +1,117 @@
+let currentOp = '';
+const clearBtn = document.querySelector('.clear');
+const topDisplay = document.querySelector('.display-top')
+const btmDisplay = document.querySelector('.display-bottom')
+const numBtns = document.querySelectorAll('button.num');
 
 
 
-function calculate(a,op,b){
-  [a,b] = [+a,+b];
+// function test(num1,op){
+//   this.op = op;
+//   this.num1 = num;
+//   return function(num2){
+//     return calculate(this.num1,this.op,num);
+//   }
+// }
+
+
+
+// console.log(calculate(5,'+','7'));  
+
+
+numBtns.forEach( btn => {
+  btn.addEventListener('click', () => {
+    updateDisplays(btn.textContent);
+  })
+})
+
+
+function splitEquation(text){
+  for (let i = 1; i < text.length; i++){
+    if (isNaN(text[i])) {
+      return [text.slice(0,i),text[i],text.slice(i+1)];
+    }
+  }
+}
+
+function updateDisplays(input){
+  if (input === '=') {
+    const [a,op,b] = splitEquation(btmDisplay.textContent);
+    const result = calculate(a,b,op);
+    updateTopDisplay();
+    updateBtmDisplay(input, result);
+    return;
+  }
+
+
+
+
+  const previousInput = btmDisplay.textContent[btmDisplay.textContent.length-1];
+  if (isNaN(previousInput) && isNaN(input) && input !== '.'){
+    // updateTopDisplay();
+    // updateBtmDisplay(input, true);
+  } else {
+    updateBtmDisplay(input);
+  }
+    
+}
+
+function doesCurrentOpExist(op){
+  if (!currentOp) { 
+    currentOp = op;
+    return false;
+  } return true;
+}
+
+function updateTopDisplay(){
+  const td = topDisplay.textContent;
+  if (td[td.length-1] === '-') topDisplay.textContent = td.slice(0,-1);
+  topDisplay.textContent = btmDisplay.textContent + '=';
+}
+
+function updateBtmDisplay(input, result = ''){
+
+  if (result) {
+    btmDisplay.textContent = result;
+    console.log(input);
+    if (input !== '=') btmDisplay.textContent += input;
+    return;
+  } 
+
+  const dsp = btmDisplay.textContent;
+  console.log(dsp);
+  if (dsp.length > 23) return; 
+  if (isNaN(dsp[dsp.length-1]) && isNaN(input)) return;
+  if (dsp === '0' && !isNaN(input)) {
+    btmDisplay.textContent = '';
+  }
+
+  btmDisplay.textContent += input
+}
+
+
+clearBtn.addEventListener('click', () => {
+  topDisplay.textContent = ' ';
+  btmDisplay.textContent = 0;
+  currentOp = '';
+})
+
+const opBtns = document.querySelectorAll('.op');
+opBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    updateDisplays(btn.textContent);
+  })
+})
+
+const equalBtn = document.querySelector('.equals');
+equalBtn.addEventListener('click', () => {
+  updateDisplays('=');
+})
+
+
+
+
+function calculate(a,b,op){
   const operations = {
     '+': (a,b) => a+b,
     '-': (a,b) => a-b,
@@ -10,8 +119,5 @@ function calculate(a,op,b){
     '÷': (a,b) => a/b,
   }
 
-  return operations[op](a,b);
+  return operations[op](+a,+b);
 }
-
-
-console.log(calculate(5,'+','7'));
